6a6ba85dec168807545c0e22a199d3699fd532fd
hyungyeom <rlagusrua3687@gmail.com>
2023-12-18 11:00:50 +0900

    :tada:Chore: 환경설정 세팅

---
+++ src/main/java/project/insta/clone/controller/TestController.java
@@ -0,0 +1,13 @@
+package project.insta.clone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "home";
+    }
+}
